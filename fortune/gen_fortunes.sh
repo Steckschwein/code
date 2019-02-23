0838d9dcfc5e9c4b2f10c842ed9bf383a2d76239
Thomas Woinke <thomas@steckschwein.de>
2019-02-23 19:40:57 +0100

    cleanup fortunes generation, deps

--- fortune/gen_fortunes.sh
+++ fortune/gen_fortunes.sh
@@ -2,6 +2,6 @@
 
 for i in {0..255} ; do
     echo fortune$i:
-    fortune -s | sed -e "s/\"//g" -e "s/$/\",\$0a/g" -e  "s/^/.byte \"/g"
+    fortune -s | sed -e "s/\t//g" -e "s/\"//g" -e "s/$/\",\$0a/g" -e  "s/^/.byte \"/g"
     echo ".byte \$0a, 0"
 done
