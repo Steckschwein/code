2cc314d2e772f3bc854a33855ac82e9d2e7e98f3
Marko Lauke <marko.lauke@googlemail.com>
2019-02-27 08:27:26 +0100

    +initial

---
+++ steckos/libsrc/zeropage.s
@@ -0,0 +1,15 @@
+;
+; zeropage usage
+
+.include        "zeropage.inc"
+
+; ------------------------------------------------------------------------
+.zeropage
+ptr1:           .res    2
+ptr2:           .res    2
+ptr3:           .res    2
+ptr4:           .res    2
+tmp1:           .res    1
+tmp2:           .res    1
+tmp3:           .res    1
+tmp4:           .res    1
