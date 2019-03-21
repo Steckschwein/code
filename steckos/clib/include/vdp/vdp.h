e3d8bf12d4ea43738fed697f0064e7f85849f448
Marko Lauke <marko.lauke@googlemail.com>
2019-03-21 09:02:02 +0100

    +fix clavier.c

--- steckos/clib/include/vdp/vdp.h
+++ steckos/clib/include/vdp/vdp.h
@@ -1,3 +1,7 @@
+#ifndef _VDP_H
+#define _VDP_H 
+
+
 #define ADDRESS_GFX1_SCREEN             0x0000			// name table
 #define ADDRESS_GFX1_PATTERN            0x3800
 #define ADDRESS_GFX1_COLOR              0x1b80
@@ -10,13 +14,13 @@
 #define ADDRESS_GFX2_SPRITE			ADDRESS_GFX_SPRITE
 #define ADDRESS_GFX2_SPRITE_PATTERN     ADDRESS_GFX1_SPRITE_PATTERN
 
-
-
-#ifndef _VDP_H
-#define _VDP_H 
 /*****************************************************************************/
 /*                                   Code                                    */
 /*****************************************************************************/
-void __fastcall__ vdp_memcpy (unsigned int vram_addr, unsigned char bytes, unsigned char *data); 
+void __fastcall__ vdp_memcpy (unsigned int vram_addr, unsigned char bytes, unsigned char *data);
+
+void __fastcall__ vdp_init (unsigned char mode);
+
+void vdp_restore();
 
 #endif 
--- steckos/clib/libsrc/vdp/vdp.s
+++ steckos/clib/libsrc/vdp/vdp.s
@@ -1,13 +1,30 @@
-;
-; void __fastcall__ vdp_memcpy (unsigned int vramadress, unsigned char count, unsigned char *data); 
-;
-
-        .import pushax, steaxspidx, incsp1, incsp3, return0
-        .importzp ptr1, tmp1, tmp2
+  .include "kernel/kernel_jumptable.inc"
+  
+  .import pushax, steaxspidx, incsp1, incsp3, return0
+  .importzp ptr1, tmp1, tmp2
+  
+  .import vdp_init_reg
+  
 
 ;----------------------------------------------------------------------------
 .code
 
+;
+; void __fastcall__ vdp_memcpy (unsigned int vramadress, unsigned char count, unsigned char *data); 
+;
+.export _vdp_memcpy
 .proc _vdp_memcpy
         rts
+.endproc
+
+
+.export _vdp_init
+.proc _vdp_init
+  jmp vdp_init_reg
+  rts
+.endproc
+
+.export _vdp_restore
+.proc _vdp_restore
+  jmp krn_textui_enable  
 .endproc
