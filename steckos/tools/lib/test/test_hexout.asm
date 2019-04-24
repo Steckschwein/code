9b79ecaff8e1967f2cfd120ddd32771da82be55e
Thomas Woinke <thomas@steckschwein.de>
2019-04-24 17:09:52 +0200

    check hexout cycles

--- steckos/tools/lib/test/test_hexout.asm
+++ steckos/tools/lib/test/test_hexout.asm
@@ -1,28 +1,35 @@
 .include "asmunit.inc" 	; unit test api
 
 .import hexout		; uut
-	
+
 .code
 
 	test "hexout"
 
 	lda	#$7e
 	jsr	hexout
-	
+
 	assertOut "7E"	; assert outpuz
 	assertA $7e		; assert A is not destroyed
 
 	lda	#$e7
 	jsr	hexout
-	
+
 	assertOut "E7"	;
 	assertA $e7		;
-	
+
 	lda	#$9f
-	
-	jsr	hexout	
-	
+
+	jsr	hexout
+
 	assertOut "9F"
-	assertA $9f	
-	
+	assertA $9f
+
+    test_name "hexout cycles"
+
+	lda	#$9f
+    resetCycles
+    jsr hexout
+    assertCycles $83  ; pass
+
 	brk
