9f8fb4706ebc9e3d9a2d2ce3d86e43f0b03839ae
Marko Lauke <marko.lauke@googlemail.com>
2019-05-04 11:28:05 +0200

    +initial

---
+++ steckos/demo/gfx6test.asm
@@ -0,0 +1,31 @@
+      .include "steckos.inc" 
+      .include "vdp.inc" 
+
+      .import vdp_gfx6_on
+      .import vdp_gfx6_blank
+      .import vdp_fill
+      
+.code
+    
+    jsr	krn_textui_disable			;disable textui 
+    
+    jsr vdp_gfx6_on
+    
+    lda #Black<<4|Black
+    jsr vdp_gfx6_blank
+    
+    vdp_vram_r ADDRESS_TEXT_PATTERN
+    vdp_vram_w ADDRESS_GFX6_SCREEN
+    lda $0300;#Cyan<<4
+    ldx #192  ;lines
+    jsr vdp_fill
+;    keyin
+    
+    jsr	krn_textui_init
+    
+    
+		jmp (retvec) 
+    
+.data
+charset:
+    .include "../../rom/bios/charset_8x8.asm"
