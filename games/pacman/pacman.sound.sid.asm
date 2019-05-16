c32e4653831ba0487ab7321db7916b1231661588
Marko Lauke <marko.lauke@googlemail.com>
2019-05-16 19:27:21 +0200

    +initial

---
+++ games/pacman/pacman.sound.sid.asm
@@ -0,0 +1,14 @@
+      .include "pacman.inc"
+      .export sound_init
+      .export sound_init_game_start
+      .export sound_play
+      .export sound_play_state
+
+      .import wait
+      .import game_state
+
+sound_init:
+sound_init_game_start:
+sound_play:
+sound_play_state:
+      rts
