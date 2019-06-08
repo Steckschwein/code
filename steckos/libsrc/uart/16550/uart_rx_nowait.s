af3fc5a3acdb5bf37b239a8122895a303d86dd7a
Thomas Woinke <thomas@steckschwein.de>
2019-06-08 18:38:07 +0200

    added uart_rx_nowait

---
+++ steckos/libsrc/uart/16550/uart_rx_nowait.s
@@ -0,0 +1,15 @@
+.include "uart.inc"
+.export uart_rx_nowait
+;----------------------------------------------------------------------------------------------
+; receive byte, no wait, set carry and store in A when received
+;----------------------------------------------------------------------------------------------
+uart_rx_nowait:
+        lda #lsr_DR
+        bit uart1+uart_lsr
+        beq @l
+        lda uart1+uart_rxtx
+        sec
+        rts
+@l:
+        clc
+        rts
