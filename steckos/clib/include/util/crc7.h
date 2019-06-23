14b63cd501c34e88490e8b04eac955c9dba22d7b
Marko Lauke <marko.lauke@googlemail.com>
2019-06-23 15:15:22 +0200

    +initial

---
+++ steckos/clib/include/util/crc7.h
@@ -0,0 +1,12 @@
+#ifndef _CRC7_H
+#define _CRC7_H
+
+
+
+/*****************************************************************************/
+/*                                   Code                                    */
+/*****************************************************************************/
+extern unsigned char __fastcall__ crc7 (unsigned char *data, unsigned char length);
+
+
+#endif _CRC7_H
