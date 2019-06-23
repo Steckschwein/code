2a684882a2266008089b85e91af2034e0ce1a487
Marko Lauke <marko.lauke@googlemail.com>
2019-06-23 15:28:04 +0200

    +update

--- steckos/tools/crc7.c
+++ steckos/tools/crc7.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 #include "include/util/crc7.h"
 
 
@@ -7,10 +8,12 @@ int main (int args, unsigned char* argv[]){
 
    unsigned char crc_res;
 
-   if(args <= 0){
+   if(args <= 1){
       return EXIT_FAILURE;
    }
-   crc_res = crc7(argv[0], strlen(argv[0]));
+   crc_res = crc7(argv[1], strlen(argv[1]));
+
+   cprintf("\n crc7 '%s' 0x%x\n", argv[1], crc_res);
 
    return EXIT_SUCCESS;
 }
