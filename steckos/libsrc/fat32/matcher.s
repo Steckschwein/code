7c4fcc8605d322c5bfe5344c44589522454ef6d7
Thomas Woinke <thomas@steckschwein.de>
2019-12-08 00:22:51 +0100

    moved matcher to lib

--- steckos/libsrc/fat32/matcher.s
+++ steckos/libsrc/fat32/matcher.s
@@ -19,9 +19,9 @@
 ; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ; SOFTWARE.
-.include	"kernel.inc"
+
 .include	"fat32.inc"
-.include	"common.inc"
+.include	"zeropage.inc"
 
 .export dirname_mask_matcher
 
