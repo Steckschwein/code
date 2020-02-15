834bf15267d89c9742800e924afc16a84fced78c
Marko Lauke <marko.lauke@googlemail.com>
2020-02-15 17:22:51 +0100

    +fix include conflicting with cc65

--- steckos/clib/libsrc/vdp/vdp.s
+++ steckos/clib/libsrc/vdp/vdp.s
@@ -1,9 +1,9 @@
   .include "kernel/kernel_jumptable.inc"
-  .include "vdp.inc"
-  
+  .include "asminc/vdp.inc"
+
   .import pushax, steaxspidx, incsp1, incsp3, return0
   .importzp ptr1, tmp1, tmp2
-  
+
   .import vdp_init_reg
   .import vdp_memcpy
   .import hexout
@@ -15,37 +15,37 @@
 .code
 
 ;
-; void __fastcall__ vdp_memcpy (unsigned char *data, unsigned int vramaddress, unsigned char pages); 
+; void __fastcall__ vdp_memcpy (unsigned char *data, unsigned int vramaddress, unsigned char pages);
 ;
 .export _vdp_memcpy
 .proc _vdp_memcpy
-		
+
 		pha ;save page count
-		
+
 		jsr popa
 		sta a_vreg
 		jsr popa
 		and #$3f
 		ora #WRITE_ADDRESS
 		sta a_vreg
-		
+
 		jsr popax
 		phx
 		ply
-		
+
 		pla ;restore page count
-		
+
 		jmp vdp_memcpy
 .endproc
 
 ; void __fastcall__ vdp_init (unsigned char mode);
 .export _vdp_init
 .proc _vdp_init
-		
+
 		jmp vdp_init_reg
 .endproc
 
 .export _vdp_restore
 .proc _vdp_restore
-		jmp krn_textui_enable  
+		jmp krn_textui_enable
 .endproc
