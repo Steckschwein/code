18891f5bdfd0c98f70cc3ea5b68e3f3c172776a1
Marko Lauke <marko.lauke@googlemail.com>
2021-09-26 00:55:09 +0200

    +initial

---
+++ steckos/libsrc/t99xx/t99xx_cmd.s
@@ -0,0 +1,36 @@
+; MIT License
+;
+; Copyright (c) 2018 Thomas Woinke, Marko Lauke, www.steckschwein.de
+;
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the "Software"), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+; copies of the Software, and to permit persons to whom the Software is
+; furnished to do so, subject to the following conditions:
+;
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+;
+; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+; SOFTWARE.
+
+.include "vdp.inc"
+
+.export vdp_wait_cmd
+
+.code
+vdp_wait_cmd:
+	 vdp_sreg 2, v_reg15			; 2 - to select status register S#2
+@wait:
+	 vdp_wait_s 4
+	 lda a_vreg
+	 ror
+	 bcs @wait
+	 vdp_sreg 0, v_reg15			; 0 - reset status register selection to S#0
+	 rts
