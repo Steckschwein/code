3c3c8e78e1b89f1613a08550f22001fa0fb14e89
Marko Lauke <marko.lauke@googlemail.com>
2021-10-01 07:40:27 +0200

    +more clean code, separate set pixel

--- steckos/libsrc/t99xx/mode2.s
+++ steckos/libsrc/t99xx/mode2.s
@@ -27,7 +27,6 @@
 
 .export vdp_mode2_on
 .export vdp_mode2_blank
-.export vdp_mode2_set_pixel
 
 .importzp vdp_ptr, vdp_tmp
 
@@ -91,54 +90,3 @@ vdp_mode2_blank:		; 2 x 6K
 	ldx #3		;768 byte screen map
 	lda #0
 	jmp vdp_fill
-
-;	set pixel to gfx2 mode screen
-;
-;	X - x coordinate [0..ff]
-;	Y - y coordinate [0..bf]
-;	A - color [0..f]
-;
-; 	VRAM ADDRESS = 8(INT(X DIV 8)) + 256(INT(Y DIV 8)) + (Y MOD 8)
-vdp_gfx2_set_pixel:
-vdp_mode2_set_pixel:
-		beq vdp_gfx2_set_pixel_e	; 0 - not set, leave blank
-		; calculate low byte vram adress
-		txa						;2
-		and	#$f8
-		sta	vdp_tmp
-		tya
-		and	#$07
-		ora	vdp_tmp
-		sta	a_vreg	;4 set vdp vram address low byte
-		sta	vdp_tmp	;3 safe vram low byte
-
-		; high byte vram address - div 8, result is vram address "page" $0000, $0100, ...
-		tya						;2
-		lsr						;2
-		lsr						;2
-		lsr						;2
-		sta	a_vreg				;set vdp vram address high byte
-		ora #WRITE_ADDRESS		;2 adjust for write
-		tay						;2 safe vram high byte for write in y
-
-		txa						;2 set the appropriate bit
-		and	#$07				;2
-		tax						;2
-		lda	bitmask,x			;4
-		ora	a_vram				;4 read current byte in vram and OR with new pixel
-		tax						;2 or value to x
-		nop						;2
-		nop						;2
-		nop						;2
-		lda	vdp_tmp			;2
-		sta a_vreg
-		tya						;2
-		nop						;2
-		nop						;2
-		sta	a_vreg
-		vdp_wait_l
-		stx a_vram	;set vdp vram address high byte
-vdp_gfx2_set_pixel_e:
-		rts
-bitmask:
-	.byte $80,$40,$20,$10,$08,$04,$02,$01
