a148c1112c36860765889bd0efb904bb8d2ae4af
mlauke <marko.lauke@googlemail.com>
2022-08-25 12:32:10 +0200

    +update

--- steckos/bios_cpld/bios.asm
+++ steckos/bios_cpld/bios.asm
@@ -2,6 +2,8 @@
 
 .code
 
+uart_cpb = $0250
+
 do_reset:
 			; disable interrupt
 			sei
@@ -11,12 +13,38 @@ do_reset:
 			; init stack pointer
 			ldx #$ff
 			txs
-:
-		lda a_vreg
-		lda $0200+5 ;lsr
-		bra	:-
 
-;.res 32768, $ea
+		lda #fcr_FIFO_enable | fcr_reset_receiver_FIFO | fcr_reset_transmit_FIFO
+		sta uart1+uart_fcr
+
+		stz uart1+uart_ier	; polled mode (so far)
+		stz uart1+uart_mcr	; reset DTR, RTS
+
+		lda #fcr_FIFO_enable | fcr_reset_receiver_FIFO | fcr_reset_transmit_FIFO
+		sta uart_cpb+uart_fcr
+		stz uart_cpb+uart_ier	; polled mode (so far)
+		stz uart_cpb+uart_mcr	; reset DTR, RTS
+
+@loop:
+		; lda uart_cpb+5 ;lsr
+
+
+		lda #lsr_THRE
+@l0:
+		bit uart_cpb+uart_lsr
+		beq @l0
+		lda #'A'
+		sta uart_cpb+uart_rxtx
+
+@l1:
+		lda #lsr_THRE
+		bit uart1+uart_lsr
+		beq @l1
+
+		lda #'X'
+		sta uart1+uart_rxtx
+
+		bra	@loop
 
 bios_irq:
 	rti
