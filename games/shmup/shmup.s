ff1d018f1ee677e4137adb94d0f47bc057060d4e
mlauke <marko.lauke@gmail.com>
2023-10-14 15:27:36 +0200

    +fix color table

--- games/shmup/shmup.s
+++ games/shmup/shmup.s
@@ -9,40 +9,41 @@ appstart $1000
 
 .code
 
-  sei
-  copypointer user_isr, save_isr
-  SetVector isr, user_isr
-  cli
+    sei
+    copypointer user_isr, save_isr
+    SetVector isr, user_isr
 
-  jsr gfxui_on
+    jsr gfxui_on
 
-  vdp_vram_w ADDRESS_GFX7_SPRITE
+    vdp_vram_w ADDRESS_GFX7_SPRITE
     lda #<sprite_attr
     ldy #>sprite_attr
-    ldx #sprite_attr_end - sprite_attr
+    ldx #(sprite_attr_end - sprite_attr)
     jsr vdp_memcpys
 
-  vdp_vram_w ADDRESS_GFX7_SPRITE_PATTERN
+    vdp_vram_w ADDRESS_GFX7_SPRITE_PATTERN
     lda #<sprite_pattern
     ldy #>sprite_pattern
-    ldx #sprite_pattern_end - sprite_pattern
+    ldx #(sprite_pattern_end - sprite_pattern)
     jsr vdp_memcpys
 
-  vdp_vram_w ADDRESS_GFX7_SPRITE_COLOR
+    vdp_vram_w ADDRESS_GFX7_SPRITE_COLOR
     lda #<sprite_color
     ldy #>sprite_color
-    ldx #4*8
-    jsr vdp_fills
+    ldx #2*16 ; 16x16 sprites
+    jsr vdp_memcpys
 
-  keyin
+    cli
 
-  jsr gfxui_off
+    keyin
 
-  sei
-  copypointer save_isr, user_isr
-  cli
+    jsr gfxui_off
 
-  jmp (retvec)
+    sei
+    copypointer save_isr, user_isr
+    cli
+
+    jmp (retvec)
 
 gfxui_on:
   jsr krn_textui_disable      ;disable textui
@@ -52,7 +53,7 @@ gfxui_on:
   vdp_sreg v_reg8_VR, v_reg8
   vdp_sreg v_reg9_ln | v_reg9_nt, v_reg9  ; 212px
 
-  ldy #$ff
+  ldy #$0f
   jsr vdp_mode7_blank
 
   rts
@@ -75,7 +76,7 @@ isr:
   bit  a_vreg
   bpl  isr_end
   lda  #%00011100
-   jsr  vdp_bgcolor
+  jsr vdp_bgcolor
 
   lda  #0
    jsr  vdp_bgcolor
@@ -85,17 +86,49 @@ isr_end:
 
 .data
 sprite_attr:
-sprite_attr_0:
+sprite_attr_1:
   sprite_y: .byte 150
   sprite_x: .byte 150
   pattern:  .byte 0
-  res: .byte 0
-  .byte SPRITE_OFF+8  ; all others off
+  .byte 0
+sprite_attr_2:
+  sprite_2_y: .byte 100
+  sprite_2_x: .byte 100
+  pattern_2:  .byte 4
+  .byte 0
+
+  .byte SPRITE_OFF+8  ; all other sprites off
 sprite_attr_end:
 
 sprite_pattern:
 sprite_0:
   .byte 255
+  .byte 254
+  .byte 255
+  .byte 255
+  .byte 254
+  .byte 255
+  .byte 1
+  .byte 3
+
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+  .byte $0
+
   .byte 255
   .byte 255
   .byte 255
@@ -103,72 +136,82 @@ sprite_0:
   .byte 255
   .byte 255
   .byte 255
-sprite_1:
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
+  .byte 255
+
 sprite_2:
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-  .byte $0
-sprite_3:
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
+
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
+
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
+
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
   .byte 255
+  .byte 220
+  .byte 100
+  .byte 50
 sprite_pattern_end:
 
 sprite_color:
-  .byte %11000000
-  .byte %11100000
-  .byte %00011100
-  .byte %00000111
-  .byte %00000111
-  .byte %00001110
-  .byte %00011100
-  .byte %00111000
+  .byte Light_Blue
+  .byte Cyan
+  .byte Light_Red
+  .byte Light_Blue
+  .byte Light_Yellow
+  .byte Dark_Green
+  .byte Gray
+  .byte White
 
-  .byte %11000000
-  .byte %11100000
-  .byte %00011100
-  .byte %00000111
-  .byte %00000111
-  .byte %00001110
-  .byte %00011100
-  .byte %00111000
+  .byte Medium_Red
+  .byte Medium_Green
+  .byte Light_Green
+  .byte Magenta
+  .byte Dark_Blue
+  .byte Dark_Yellow
+  .byte Dark_Red
+  .byte Gray
+; sprite 2 color
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
 
-  .byte %11000000
-  .byte %11100000
-  .byte %00011100
-  .byte %00000111
-  .byte %00000111
-  .byte %00001110
-  .byte %00011100
-  .byte %00111000
-
-  .byte %11000000
-  .byte %11100000
-  .byte %00011100
-  .byte %00000111
-  .byte %00000111
-  .byte %00001110
-  .byte %00011100
-  .byte %00111000
-
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
+  .byte White
 sprite_color_end:
 
 .bss
--- steckos/libsrc/t99xx/mode7.s
+++ steckos/libsrc/t99xx/mode7.s
@@ -42,9 +42,9 @@ vdp_gfx7_on:
 			jmp vdp_init_reg
 
 vdp_init_bytes_gfx7:
-			.byte v_reg0_m5|v_reg0_m4|v_reg0_m3									; reg0 mode bits
+			.byte v_reg0_m5|v_reg0_m4|v_reg0_m3									        ; reg0 mode bits
 			.byte v_reg1_display_on|v_reg1_spr_size |v_reg1_int 				; TODO FIXME verify v_reg1_16k t9929 specific, therefore 0
-			.byte >(ADDRESS_GFX7_SCREEN>>3) | $1f	; => 00<A16>1 1111 - entw. bank 0 (offset $0000) or 1 (offset $10000)
+			.byte >(ADDRESS_GFX7_SCREEN>>3 | $3f)	; => 00<A16>1 1111 - entw. bank 0 (offset $0000) or 1 (offset $10000)
 			.byte 0
 			.byte 0
 			.byte	>(ADDRESS_GFX7_SPRITE<<1 | $07)   ; sprite attribute table => $07 -> see V9938_MSX-Video_Technical_Data_Book_Aug85.pdf S.93
@@ -52,7 +52,7 @@ vdp_init_bytes_gfx7:
 			.byte %00000000 ; border color
 			.byte v_reg8_SPD | v_reg8_VR	; SPD - sprite disabled, VR - 64k VRAM  - R#8
 			.byte v_reg9_nt ; #R9, set bit to 1 for PAL
-			.byte .hiword(ADDRESS_GFX7_SPRITE_COLOR<<2) ; color table high, a16-14
+			.byte 0
 			.byte >(ADDRESS_GFX7_SPRITE >> 7)
 			.byte 0;  #R12
 			.byte 0;  #R13
