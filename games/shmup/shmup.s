8fe5abc34d82f2ce0a765cde8d7cb7e6d5999247
Thomas Woinke <thomas@steckschwein.de>
2023-10-22 22:13:47 +0200

    moved hextodec to lib

--- games/shmup/shmup.s
+++ games/shmup/shmup.s
@@ -186,28 +186,6 @@ sprity_mc_spriteface:
    
   rts
 
-; hextodec 8bit hex to decimal converter
-; from https://x.com/mrdoornbos/status/1715326394970862020?s=20
-    ; accumulator
-    ; to
-    ; y - hundreds
-    ; x - tens
-    ; a - ones
-hextodec:
-    ldy #$2f 
-    ldx #$3a
-    sec 
-:
-    iny
-    sbc #100
-    bcs :- 
-:
-    dex
-    adc #10
-    bmi :-
-    adc #$2f 
-    rts
-
 .data
 sprite_attr:
   sprite_0_y: .byte 0
