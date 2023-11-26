4fd3ae9fc41628a8dccfa2ab3affbcb4c280feeb
Thomas Woinke <thomas@steckschwein.de>
2023-11-26 14:52:59 +0100

    cleanup

--- mkimg.sh
+++ mkimg.sh
@@ -4,13 +4,14 @@ sectors_clus=4
 alignment=1048576
 
 img="steckos.img"
+
+
+if [ -e ${img} ] ; then
+	rm ${img}
+fi
+
 truncate -s $((size * (1<<20) )) "${img}"
 parted --machine --script "${img}" mklabel msdos mkpart primary fat32 "${alignment}B" '100%' 
 mformat -i "${img}"@@"${alignment}" -c $sectors_clus -F -t $((size>>20))  #-v "steckos"
-foo=$(sudo kpartx -av "$img")
-if [ $? -ne 0 ] ; then
-    exit 1
-fi
-
 mcopy -i "${img}"@@"${alignment}" -s dist/* ::/
 
