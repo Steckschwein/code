db811d929e4dd6ec1c923448d20e0bb94ec9a2d4
Thomas Woinke <thomas@steckschwein.de>
2023-11-26 14:33:35 +0100

    use parted and mtools for image creation

--- mkimg.sh
+++ mkimg.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
-size=128
+size=256
+sectors_clus=4
+alignment=1048576
 
 img="steckos.img"
-
-if [ -e $img ] ; then
-    echo "Image $img already exists"
-    exit 1
-else
-    echo "Creating image $img"
-    dd if=/dev/zero of=$img bs=1024k count=$size
-fi
-
-printf 'o\nn\np\n1\n\n\nt\nc\nw\n' | fdisk $img
+truncate -s $((size * (1<<20) )) "${img}"
+parted --machine --script "${img}" mklabel msdos mkpart primary fat32 "${alignment}B" '100%' 
+mformat -i "${img}"@@"${alignment}" -c $sectors_clus -F -t $((size>>20))  #-v "steckos"
 foo=$(sudo kpartx -av "$img")
 if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-loopdev="/dev/mapper/"$(echo $foo | cut -d ' ' -f3)
-echo $loopdev
-sudo mkfs -t fat -F 32 -s 16 -S 512 $loopdev
-sudo mkdir image
-sudo mount $loopdev image
-sudo rsync -rv dist/ image/
-sudo umount image
-sudo kpartx -dv "$img"
-sudo rm -fr image
+mcopy -i "${img}"@@"${alignment}" -s dist/* ::/
+
