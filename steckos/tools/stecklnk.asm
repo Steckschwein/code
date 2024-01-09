3350b14ecaf686a0a7bd9ee8b6f223c0dece9802
mlauke <marko.lauke@gmail.com>
2024-01-09 21:23:14 +0100

    ref #33 +enhance to y-modem +impl. stecklnk

--- steckos/tools/stecklnk.asm
+++ steckos/tools/stecklnk.asm
@@ -37,6 +37,7 @@
 
 appstart $1000
 
+      bra @start
       lda (paramptr)
       bne :+
       jsr primm
@@ -47,19 +48,30 @@ appstart $1000
 
 :     lda paramptr
       ldx paramptr+1
+@file: .asciiz "foo.txt"
+@start:
+      lda #<@file
+      ldx #>@file
       ldy #O_CREAT
       jsr krn_open
       bcs @l_exit_err
       stx fd
 
+      stz status
       jsr primm
       .byte "Stecklink ", 0
 
       lda #<handle_block
       ldx #>handle_block
-      jsr xmodem_upload_callback
+      jsr ymodem_upload_callback
+      bcc :+
+      pha
+      jsr primm
+      .byte "ymodem i/o error",0
+      pla
+      jsr hexout_s
 
-      ldx fd
+:     ldx fd
       jsr krn_close
 
 @l_exit:
@@ -74,24 +86,71 @@ appstart $1000
       bra @l_exit
 
 
+; xmodem callback
+; in:
+;   A - block number
+;   X - offset in data recv buffer until XMODEM_DATA_END
 handle_block:
+
+      cmp #0
+      bne @data_block
+      ldy status
+      bne @data_block
+      inc status
+
+      ; 7a 6d 6f 64 65 6d 2e 74 78 74 00
+      jsr primm
+      .byte CODE_LF, "file: ",0
+:     lda xmodem_rcvbuffer,x
+      beq @l_fsize
+      jsr char_out
+      inx
+      cpx #XMODEM_DATA_END
+      bne :-
+      bra @l_exit
+@l_fsize:
+      ; 31 33 20
+      jsr primm
+      .byte CODE_LF,"size: ",0
+      inx
+      jsr parseOctal
+      bcs @l_exit
+@l_modts:
+      ; 31 34 35 34 37 30 31 37 35 34 33 20
+      jsr primm
+      .byte CODE_LF,"modify: ",0
+      jsr parseOctal
+      lda #CODE_LF
+      jsr char_out
+      bra @l_exit
+
+@data_block:
       ldy crs_x ; save crs x
       phy
-      pha
-
 @copy:
-      lda xmodem_rcvbuffer,x
       phx
-      ldx fd
-      jsr krn_write_byte
-      plx
-      bcs @l_exit
+      lda xmodem_rcvbuffer,x
+ ;     jsr hexout_s
+      lda #' '
+;      jsr char_out
+ ;     ldx fd
+;      jsr krn_write_byte
+;      bcs @l_exit
       _inc32 bytes
+      plx
       inx
       cpx #XMODEM_DATA_END
       bne @copy
 
-@l_exit:
+;      pha
+;      jsr primm
+ ;     .byte "block: ", 0
+ ;     pla
+;      jsr hexout_s
+
+      jsr primm
+      .byte "bytes: ", 0
+
       lda bytes+3
       jsr hexout_s
       lda bytes+2
@@ -102,14 +161,28 @@ handle_block:
       jsr hexout
 
       pla
-      pla
       sta crs_x
       jmp krn_textui_update_crs_ptr
+@l_exit:
+      rts
+
+parseOctal:
+:     lda xmodem_rcvbuffer,x
+      inx
+      cmp #$20
+      beq :+
+      jsr char_out
+      cpx #XMODEM_DATA_END
+      bne :-
+      rts
+:     clc
+      rts
 
 .data
   bytes: .res 4, 0
 
 .bss
+  status: .res 1
   fd: .res 1
   crc16_l: .res 256
   crc16_h: .res 256
