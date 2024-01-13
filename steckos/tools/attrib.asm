a7b943906a280c95c9fbdb9c4f8b0451e620df40
Thomas Woinke <thomas@steckschwein.de>
2024-01-13 17:08:20 +0100

    use print_attribs from fsutil

--- steckos/tools/attrib.asm
+++ steckos/tools/attrib.asm
@@ -139,18 +139,20 @@ attrib:
 		lda #':'
 		jsr krn_chrout
 
-		lda (dirptr),y
-		ldx #$03
-@al:
-		bit attr_tbl,x
-		beq @skip
-		pha
-		lda attr_lbl,x
-		jsr krn_chrout
-		pla
-@skip:
-		dex
-		bpl @al
+		jsr print_attribs
+
+; 		lda (dirptr),y
+; 		ldx #$03
+; @al:
+; 		bit attr_tbl,x
+; 		beq @skip
+; 		pha
+; 		lda attr_lbl,x
+; 		jsr krn_chrout
+; 		pla
+; @skip:
+; 		dex
+; 		bpl @al
 @out:
 		jmp (retvec)
 
