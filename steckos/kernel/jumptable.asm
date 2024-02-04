ff9062db2e235c97472f08caa841a7a0e61e9d91
Thomas Woinke <thomas@steckschwein.de>
2024-02-04 22:05:50 +0100

    find first/find next

--- steckos/kernel/jumptable.asm
+++ steckos/kernel/jumptable.asm
@@ -36,10 +36,10 @@ krn_fread_byte:         jmp fat_fread_byte
 krn_write_byte:         jmp fat_write_byte
 .export krn_fseek
 krn_fseek:              jmp fat_fseek
-; .export krn_find_first
-; krn_find_first:         jmp fat_find_first
-; .export krn_find_next
-; krn_find_next:          jmp fat_find_next
+.export krn_find_first
+krn_find_first:         jmp fat_find_first
+.export krn_find_next
+krn_find_next:          jmp fat_find_next
 .export krn_getcwd
 krn_getcwd:             jmp fat_get_root_and_pwd
 .export krn_readdir
