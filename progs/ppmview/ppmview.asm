651f416724fb4853d6c6dc95297348d5e1f9f1aa
mlauke <marko.lauke@gmail.com>
2024-02-14 16:38:53 +0100

    +slideshow

--- progs/ppmview/ppmview.asm
+++ progs/ppmview/ppmview.asm
@@ -33,47 +33,63 @@
 .export fread_byte=krn_fread_byte
 .export fclose=krn_close
 
+.zeropage
+  p_option: .res 2
+
 appstart $1000
 .code
-    lda (paramptr)
-    bne @l_openfile
 
+    stz _slide_delay
+
+    jsr parseOpt
+    bne @l_select_opt
+    lda (paramptr)      ; param given at all?
+    bne @l_open_file    ; yes, try open as file
+    bra @l_open_dir     ; open dir otherwise
+
+@l_select_opt:
+    SetVector _opt_slide, p_option
+    jsr is_option
+    bcc @l_open_file
+    lda #$88
+    sta _slide_delay
+
+@l_open_dir:
     lda #<_cwd
     ldy #>_cwd
     ldx #(_cwd_end-_cwd)
     jsr krn_getcwd
+    bcs exit
 
     lda #<_cwd
     ldx #>_cwd
     jsr krn_opendir
     bcs io_error
-    stx fd_dir
+    stx _fd_dir
 
     jsr gfxui_on
 @l_next_ppm:
     jsr gfxui_blank
     jsr next_ppm_file
-    bcs @l_closedir
+    bcs @l_close_dir
 
     lda #<_ppmfile
     ldx #>_ppmfile
     jsr ppm_load_image
     bcs ppm_error
 
-    keyin
-    cmp #KEY_ESCAPE
-    beq @l_closedir
+    jsr slide_show
+    bcc @l_next_ppm
 
-    bra @l_next_ppm
-
-@l_closedir:
-    ldx fd_dir
+@l_close_dir:
+    ldx _fd_dir
     jsr krn_close
     bra exit
 
-@l_openfile:
-    lda #<paramptr
-    ldx #>paramptr+1
+@l_open_file:
+    jsr gfxui_on
+    lda paramptr
+    ldx paramptr+1
     jsr ppm_load_image
     bcs ppm_error
     keyin
@@ -85,7 +101,7 @@ exit:
 io_error:
     pha
     jsr primm
-    .byte CODE_LF,"i/o error: ",0
+    .byte CODE_LF, "i/o error: ", 0
     pla
     jsr hexout_s
     lda #CODE_LF
@@ -100,15 +116,77 @@ ppm_error:
     .byte CODE_LF,"Not a valid ppm file! Must be type P6 with 256x192px and 8bpp colors.",CODE_LF,0
     bra exit
 
+slide_show:
+              lda _slide_delay
+              bpl @l_kbdhit
+
+              and #$7f
+              tay
+@l_wait:      sys_delay_ms 1000
+              phy
+              jsr krn_getkey
+              ply
+              bcc :+
+              cmp #KEY_ESCAPE
+              beq @l_exit
+:             dey
+              bne @l_wait
+@l_next:      clc
+              rts
+
+@l_kbdhit:    keyin
+@l_kbd_esc:   cmp #KEY_ESCAPE
+              bne @l_next
+@l_exit:      rts
+
+parseOpt:
+              ldy #0
+              ldx #0
+:             lda (paramptr),y
+              beq @l_exit
+              cmp #' '
+              bne @l_opt
+              cpx #0
+              beq @l_next
+              bra @l_exit
+@l_opt:       cpx #0
+              bne @l_capture
+              cmp #'-'
+              bne @l_exit
+@l_capture:   sta _option,x
+              inx
+@l_next:      iny
+              cpx #_option_end-_option
+              bne :-
+@l_exit:      stz _option,x
+              cpx #0
+              rts
+
+is_option:
+              ldy #0
+:             lda _option,y
+              beq @l_exit
+              cmp (p_option),y
+              bne @l_noop
+              iny
+              cpy #(_option_end-_option)
+              bne :-
+@l_noop:      clc
+              rts
+@l_exit:      cpy #2
+              rts
+
+
+
 next_ppm_file:
-@l_next:      lda #<direntry
-              ldy #>direntry
-              ldx fd_dir
+@l_next:      lda #<_direntry
+              ldy #>_direntry
+              ldx _fd_dir
               jsr krn_readdir
               bcs @l_exit
               ldy #F32DirEntry::Ext
               ldx #0
-:             lda direntry,y
+:             lda _direntry,y
               cmp _ppmext,x
               bne @l_next
               iny
@@ -118,7 +196,7 @@ next_ppm_file:
 
               ldy #0
               ldx #0
-:             lda direntry,y
+:             lda _direntry,y
               cmp #' '
               beq @skip
               sta _ppmfile,x
@@ -162,10 +240,14 @@ gfxui_off:
 .data
   _ppmext:  .byte "PPM"
   _ppmext_end:
+  _opt_slide: .asciiz "-slide"
 
 .bss
-fd_dir:   .res 1
-direntry: .res DIR_Entry_Size
-_cwd:     .res 64
-_cwd_end:
-_ppmfile: .res 32
+  _slide_delay: .res 1
+  _fd_dir:   .res 1
+  _direntry: .res DIR_Entry_Size
+  _cwd:     .res 64
+  _cwd_end:
+  _ppmfile: .res 12
+  _option:  .res 16
+  _option_end:
