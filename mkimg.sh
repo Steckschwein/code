30988d91655a5b001947438d5662f794839fbdde
mlauke <marko.lauke@gmail.com>
2024-03-08 17:50:02 +0100

    +rugg feldman bench

--- mkimg.sh
+++ mkimg.sh
@@ -12,14 +12,13 @@ fi
 truncate -s $size ${img}
 mkfs -t fat -F 32 -s 2  ${img} -n ${version^^}
 
-mmd -i ${img} ::/steckos ::/demo ::/games ::/progs ::/basic
+mmd -i ${img} ::/steckos ::/demo ::/games ::/progs ::/basic ::/basic/benchmrk
 
 mcopy -i ${img}  steckos/kernel/loader.prg $TARGET/
 mcopy -i ${img}  steckos/shell/shell.prg $TARGET/steckos/
 mcopy -i ${img}  steckos/tools/*.prg $TARGET/steckos/
 mcopy -i ${img}  steckos/demo/*.prg $TARGET/demo/
 mcopy -i ${img}  steckos/demo/*.ppm $TARGET/demo/
-
 
 GAMES="games/dinosaur/dinosaur.prg games/pong/pong.prg games/microchess/mchess.prg"
 mcopy -i ${img} $GAMES $TARGET/games/
@@ -28,6 +27,7 @@ PROGS=$(find progs -name "*.prg")
 mcopy -i ${img} $PROGS $TARGET/progs/
 
 mcopy -i ${img} progs/ehbasic_65c02/demo/* $TARGET/basic
+mcopy -i ${img} progs/ehbasic_65c02/benchmark/* $TARGET/basic/benchmrk
 
 if [ -d "../local" ] ; then
 	mcopy -snvom -i ${img} ../local/* $TARGET/
---
+++ progs/ehbasic_65c02/benchmark/bench8.bas
@@ -0,0 +1,21 @@
+REM https://en.wikipedia.org/wiki/Rugg/Feldman_benchmarks
+300 PRINT"S"
+400 K=0
+500 K=K+1
+530 A=K^2
+540 B=LOG(K)
+550 C=SIN(K)
+600 IF K<100 THEN 500
+700 PRINT"E"
+800 END
+
+
+300 ?"s"
+400 k=0
+500 k=k+1
+530 a=k^2
+540 b=log(k)
+550 c=sin(k)
+600 if k<100 then 500
+700 ?"e"
+800 end
