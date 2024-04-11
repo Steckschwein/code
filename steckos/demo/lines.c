f0e05779278fa4b298a6f1b6404a95df7bbdd674
Thomas Woinke <thomas@steckschwein.de>
2024-04-11 19:18:51 +0200

    lines demo

---
+++ steckos/demo/lines.c
@@ -0,0 +1,22 @@
+#include <graphics.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+  int i;
+
+  initgraph(NULL, 7, NULL);
+  cleardevice();
+
+  // setbkcolor(BLACK);
+  cleardevice();
+  outtextxy(0, 0, "Drawing 1000 lines...");
+  for (i = 0; i < 1000; i++) {
+    setcolor(1 + random(15));
+    line(random(getmaxx()), random(getmaxy()), 
+         random(getmaxx()), random(getmaxy()));
+  }
+  getch();
+  closegraph();
+  return 0;
+}
