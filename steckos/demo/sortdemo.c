46d1e6ebc9f3c12691dd300c903dc429b404e215
Thomas Woinke <thomas@steckschwein.de>
2024-04-12 16:14:27 +0200

    radix sort

--- steckos/demo/sortdemo.c
+++ steckos/demo/sortdemo.c
@@ -302,7 +302,7 @@ void heapify(unsigned char arr[], int N, int i)
 // Main function to do heap sort
 void heapSort(unsigned char arr[], int N)
 {
-    int i,j;
+    int i;
     // Build max heap
     for (i = N / 2 - 1; i >= 0; i--)
 
@@ -395,6 +395,73 @@ void selectionSort(unsigned char arr[], int n)
            }
     } 
 } 
+
+// A utility function to get maximum
+// value in arr[]
+int getMax(unsigned char arr[], int n)
+{
+    int i;
+    int mx = arr[0];
+    for (i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+ 
+// A function to do counting sort of arr[]
+// according to the digit
+// represented by exp.
+void countSort(unsigned char arr[], int n, int exp)
+{
+ 
+    // Output array
+    int output[512];
+    int i, count[10] = { 0 };
+ 
+    // Store count of occurrences
+    // in count[]
+    for (i = 0; i < n; i++)
+        count[(arr[i] / exp) % 10]++;
+ 
+    // Change count[i] so that count[i]
+    // now contains actual position
+    // of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+ 
+    // Build the output array
+    for (i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+       
+        count[(arr[i] / exp) % 10]--;
+    }
+ 
+    // Copy the output array to arr[],
+    // so that arr[] now contains sorted
+    // numbers according to current digit
+    for (i = 0; i < n; i++)
+    {
+        arr[i] = output[i];
+        setLine(i, arr[i], WHITE);
+    }
+}
+ 
+// The main function to that sorts arr[]
+// of size n using Radix Sort
+void radixsort(unsigned char arr[], int n)
+{
+    int exp;
+    // Find the maximum number to
+    // know number of digits
+    int m = getMax(arr, n);
+    
+    // Do counting sort for every digit.
+    // Note that instead of passing digit
+    // number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (exp = 1; m / exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+}
   
 
 void titleCard(char *title, int x, int y, int delay)
@@ -425,6 +492,7 @@ int main()
         max_x = getmaxx();
         cleardevice();
         n = 64;
+        n = max_x;
 
         titleCard("Sort Demo", 90, 100, 20);
 
@@ -455,7 +523,6 @@ int main()
         n = max_x;
 
         titleCard("Comb Sort - 255 values", 50, 100, 20);
-
         shuffleArray(arr, n);   
         combSort(arr, n);
         drawArray(arr, n, GREEN);
@@ -513,6 +580,11 @@ int main()
         drawArray(arr, n, GREEN);
         waitframes(50);
 
+        titleCard("Radix Sort - 512 values", 50, 100, 20);        
+        shuffleArray(arr, n);   
+        radixsort(arr, n);
+        drawArray(arr, n, GREEN);
+        waitframes(50);
 
     }
 
