e0d9ac65565dadecf4c6efa655a19ce37468d15f
Thomas Woinke <thomas@steckschwein.de>
2024-04-12 16:23:34 +0200

    premature optimization

--- steckos/demo/lines.c
+++ steckos/demo/lines.c
@@ -3,17 +3,20 @@
 
 int main (int argc, char *argv[])
 {
-  int i;
+  int i, maxx, maxy;
 
   initgraph(NULL, 6, NULL);
+
+  maxx = getmaxx();
+  maxy = getmaxy();
 
   setbkcolor(BLACK);
   cleardevice();
   outtextxy(0, 0, "Drawing 1000 lines...");
   for (i = 0; i < 1000; i++) {
     setcolor(1 + random(15));
-    line(random(getmaxx()), random(getmaxy()),
-         random(getmaxx()), random(getmaxy()));
+    line(random(maxx), random(maxy),
+         random(maxx), random(maxy));
   }
   getch();
   closegraph();
